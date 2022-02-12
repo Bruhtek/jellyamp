cfd54b6976ce85646fefdfdb5d02a4aa1f226bd1
Bruhtek <bartosz.kas2005@gmail.com>
2022-02-12 18:02:13 +0100

    Add a preparation for albums selection and play

--- lib/screens/albums/albums.dart
+++ lib/screens/albums/albums.dart
@@ -19,69 +19,11 @@ class Albums extends StatelessWidget {
   }
 }
 
-Viewtype _viewtype = Viewtype.grid;
-
-class AlbumsPage extends StatefulWidget {
-  const AlbumsPage({Key? key}) : super(key: key);
-
-  @override
-  _AlbumsPageState createState() => _AlbumsPageState();
-}
-
-class _AlbumsPageState extends State<AlbumsPage> {
-  Widget popupViewtype() {
-    const List<Icon> iconsViewtype = [
-      Icon(Icons.view_list_rounded),
-      Icon(Icons.grid_view_rounded),
-    ];
-
-    return PopupMenuButton<Viewtype>(
-      onSelected: (Viewtype result) {
-        setState(() {
-          _viewtype = result;
-        });
-      },
-      icon: iconsViewtype[_viewtype.toInt()],
-      itemBuilder: (BuildContext context) => [
-        PopupMenuItem<Viewtype>(
-          value: Viewtype.list,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              iconsViewtype[Viewtype.list.toInt()],
-              const Text('List'),
-            ],
-          ),
-        ),
-        PopupMenuItem<Viewtype>(
-          value: Viewtype.grid,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              iconsViewtype[Viewtype.grid.toInt()],
-              const Text('Grid'),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  //TODO: make a popup menu for the sort type, like the popupViewtype one
-  Widget popupSort() {
-    return Container();
-  }
+class AlbumsPage extends StatelessWidget {
+  const AlbumsPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Albums'),
-        actions: [
-          popupViewtype(),
-        ],
-      ),
-      body: AlbumsList(_viewtype),
-    );
+    return const AlbumsList();
   }
 }
