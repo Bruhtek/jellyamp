665fcc0b18f8567a67889c1a9e5cdc5a1852d03b
Bruhtek <bartosz.kas2005@gmail.com>
2022-03-18 20:44:20 +0100

    Add artists display

--- lib/screens/music/artists.dart
+++ lib/screens/music/artists.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../main.dart';
+import 'utilities/grid.dart';
+
+int crossAxisCount = 2;
+
 class ArtistsScreen extends ConsumerWidget {
   ArtistsScreen(this.toggleSelecting, {Key? key}) : super(key: key);
 
@@ -8,8 +13,24 @@ class ArtistsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text("artists"),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          crossAxisCount: crossAxisCount,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: ref
+            .watch(jellyfinAPIProvider.select((value) => value.artistsCount)),
+        itemBuilder: (context, index) {
+          final artist = ref.watch(
+              jellyfinAPIProvider.select((value) => value.getArtists()[index]));
+
+          return gridItem(context, artistCover(artist, ref), artist.name, null);
+        },
+      ),
     );
   }
 }
