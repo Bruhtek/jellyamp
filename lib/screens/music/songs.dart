13e43e9ed7bb986574f0b3700a759d3bd9b2ca89
Bruhtek <bartosz.kas2005@gmail.com>
2022-03-18 20:44:27 +0100

    Add songs display

--- lib/screens/music/songs.dart
+++ lib/screens/music/songs.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../main.dart';
+import 'utilities/grid.dart';
+
+int crossAxisCount = 2;
+
 class SongsScreen extends ConsumerWidget {
   SongsScreen(this.toggleSelecting, {Key? key}) : super(key: key);
 
@@ -8,8 +13,29 @@ class SongsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text("songs"),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          crossAxisCount: crossAxisCount,
+          childAspectRatio: 1.0,
+        ),
+        itemCount:
+            ref.watch(jellyfinAPIProvider.select((value) => value.songsCount)),
+        itemBuilder: (context, index) {
+          final song = ref.watch(
+              jellyfinAPIProvider.select((value) => value.getSongs()[index]));
+
+          return gridItem(
+            context,
+            songCover(song, ref),
+            song.title,
+            song.artistNames.join(', '),
+          );
+        },
+      ),
     );
   }
 }
