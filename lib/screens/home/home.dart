13146d44572cc0c0ee8ace9349283b52bb041b1d
Bruhtek <bartosz.kas2005@gmail.com>
2022-03-22 22:43:42 +0100

    Update home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,87 +8,195 @@ import '../../utilities/grid.dart';
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  Text stylishHomeText(String text) {
-    return Text(
-      text,
-      textAlign: TextAlign.left,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.black,
+  Widget stylishHomeText(String text) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 24.0,
+        ),
+      ),
+    );
+  }
+
+  Widget placeHolderContainer() {
+    return Container(
+      color: Colors.black12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.info_outline,
+            color: Colors.blue,
+            size: 100,
+          ),
+          stylishHomeText('Placeholder'),
+        ],
       ),
     );
   }
 
   Widget rediscoverAlbums(BuildContext context, WidgetRef ref, int count) {
-    List<Album> albums = ref.read(jellyfinAPIProvider).getRandomAlbums(count: count);
+    List<Album> albums = ref.read(jellyfinAPIProvider).getRecommendedAlbums();
 
-    return Column(
-      children: [
-        stylishHomeText("Rediscover Albums"),
-        SizedBox(
-          height: 120.0,
-          child: ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0),
-            separatorBuilder: ((context, index) => Container(width: 4.0)),
-            scrollDirection: Axis.horizontal,
-            itemCount: count,
-            itemBuilder: (context, index) {
-              final Album album = albums[index];
+    return Container(
+      color: Colors.black12,
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          stylishHomeText("Rediscover Albums"),
+          SizedBox(
+            height: 120.0,
+            child: ShaderMask(
+              shaderCallback: (Rect rect) {
+                return const LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Colors.purple,
+                    Colors.black38,
+                    Colors.transparent,
+                    Colors.transparent,
+                    Colors.black38,
+                    Colors.purple,
+                  ],
+                  stops: [
+                    0.0,
+                    0.02,
+                    0.03,
+                    0.97,
+                    0.98,
+                    1.0,
+                  ],
+                ).createShader(rect);
+              },
+              blendMode: BlendMode.dstOut,
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                separatorBuilder: ((context, index) => Container(width: 4.0)),
+                scrollDirection: Axis.horizontal,
+                itemCount: count,
+                itemBuilder: (context, index) {
+                  final Album album = albums[index];
 
-              return gridItem(
-                  context, albumCover(album, ref), album.title, album.artistNames.join(', '));
-            },
+                  return gridItem(
+                    context,
+                    albumCover(album, ref),
+                    album.title,
+                    album.artistNames.join(', '),
+                  );
+                },
+              ),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
   Widget rediscoverArtists(BuildContext context, WidgetRef ref, int count) {
-    List<Artist> artists = ref.read(jellyfinAPIProvider).getRandomArtists(count: count);
+    List<Artist> artists = ref.read(jellyfinAPIProvider).getRecommendedArtists();
 
-    return Column(
-      children: [
-        stylishHomeText("Rediscover Artists"),
-        SizedBox(
-          height: 120.0,
-          child: ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0),
-            separatorBuilder: ((context, index) => Container(width: 4.0)),
-            scrollDirection: Axis.horizontal,
-            itemCount: count,
-            itemBuilder: (context, index) {
-              final Artist artist = artists[index];
+    return Container(
+      color: Colors.black12,
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          stylishHomeText("Rediscover Artists"),
+          SizedBox(
+            height: 120.0,
+            child: ShaderMask(
+              shaderCallback: (Rect rect) {
+                return const LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Colors.purple,
+                    Colors.black38,
+                    Colors.transparent,
+                    Colors.transparent,
+                    Colors.black38,
+                    Colors.purple,
+                  ],
+                  stops: [
+                    0.0,
+                    0.02,
+                    0.03,
+                    0.97,
+                    0.98,
+                    1.0,
+                  ],
+                ).createShader(rect);
+              },
+              blendMode: BlendMode.dstOut,
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                separatorBuilder: ((context, index) => Container(width: 4.0)),
+                scrollDirection: Axis.horizontal,
+                itemCount: count,
+                itemBuilder: (context, index) {
+                  final Artist artist = artists[index];
 
-              return gridItem(context, artistCover(artist, ref), artist.name, null);
-            },
+                  return gridItem(context, artistCover(artist, ref), artist.name, null);
+                },
+              ),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
   Widget rediscoverSongs(BuildContext context, WidgetRef ref, int count) {
-    List<Song> songs = ref.read(jellyfinAPIProvider).getRandomSongs(count: count);
+    List<Song> songs = ref.read(jellyfinAPIProvider).getRecommendedSongs();
 
-    return Column(
-      children: [
-        stylishHomeText("Rediscover Songs"),
-        SizedBox(
-          height: 120.0,
-          child: ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0),
-            separatorBuilder: ((context, index) => Container(width: 4.0)),
-            scrollDirection: Axis.horizontal,
-            itemCount: count,
-            itemBuilder: (context, index) {
-              final Song song = songs[index];
+    return Container(
+      color: Colors.black12,
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          stylishHomeText("Rediscover Songs"),
+          SizedBox(
+            height: 120.0,
+            child: ShaderMask(
+              shaderCallback: (Rect rect) {
+                return const LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Colors.purple,
+                    Colors.black38,
+                    Colors.transparent,
+                    Colors.transparent,
+                    Colors.black38,
+                    Colors.purple,
+                  ],
+                  stops: [
+                    0.0,
+                    0.02,
+                    0.03,
+                    0.97,
+                    0.98,
+                    1.0,
+                  ],
+                ).createShader(rect);
+              },
+              blendMode: BlendMode.dstOut,
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                separatorBuilder: ((context, index) => Container(width: 4.0)),
+                scrollDirection: Axis.horizontal,
+                itemCount: count,
+                itemBuilder: (context, index) {
+                  final Song song = songs[index];
 
-              return gridItem(
-                  context, songCover(song, ref), song.title, song.artistNames.join(', '));
-            },
+                  return gridItem(
+                      context, songCover(song, ref), song.title, song.artistNames.join(', '));
+                },
+              ),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 
@@ -96,6 +204,7 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     List<Widget> homeWidgets = [];
 
+    homeWidgets.add(placeHolderContainer());
     homeWidgets.add(rediscoverAlbums(context, ref, 20));
     homeWidgets.add(rediscoverArtists(context, ref, 20));
     homeWidgets.add(rediscoverSongs(context, ref, 20));
@@ -104,14 +213,10 @@ class HomeScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text("Jellyamp"),
       ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 16.0),
-        // child: ListView(
-        //   children: homeWidgets,
-        // ),
-        child: ListView(
-          children: homeWidgets,
-        ),
+      body: ListView.separated(
+        itemCount: homeWidgets.length,
+        itemBuilder: ((context, index) => homeWidgets[index]),
+        separatorBuilder: ((context, index) => Container(height: 16.0)),
       ),
     );
   }
